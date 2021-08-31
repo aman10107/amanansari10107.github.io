2cd271ad8c74647c023dccc74c6e574290d1cadf
amanansari10106 <69719509+amanansari10106@users.noreply.github.com>
2021-08-31 12:40:34 +0530

    first commit

---
+++ assets/home/home.php
@@ -0,0 +1,286 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      
+       <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="format-detection" content="telephone=no">
+<meta name="theme-color" content="#75dab4"/>
+<title>BB Barman Academy : How to Open a School in India | Opening a School in India</title>
+<meta name="author" content="BB Barman Academy">
+<meta name="description" content="BB Barman Academy is the India's Leading Education Management Company helps to setup schools and colleges in India.We suggest school concept and also provide support services for establishment and management of the new school.We work with school promoters of the new CBSE, ICSE, IB and IGCSE schools.">
+<meta name="keywords" content="International School Consultants in india, Consultants for School Projects in india, Consultants for setting up schools in india, Educational Consultants in india, IB School Consultants in india, School Consultants in India, CBSE School Consultants in india, ICSE school Consultants in india, IGCSE School Consultants in india,">
+
+<!-- META TAGS -->
+<meta name="format-detection" content="telephone=no">
+<meta name=distribution content=global>
+<meta name=revisit-after content="Daily">
+<meta name=rating content="general">
+<meta name=robots content="index, follow">
+<meta name=googlebot content="index, follow ">
+<meta name=bingbot content=" index, follow ">
+<meta name="abstract" content="BB Barman Academy "></meta>
+<meta name="alexa" content="100"></meta>
+
+
+<!-- SOCIAL MEDIA META -->
+<meta property="og:description" content="BB Barman Academy | Opening a School in India">
+<meta property="og:image" content="assets/img/logo.png">
+<meta property="og:site_name" content="BB Barman Academy">
+<meta property="og:title" content="BB Barman Academy">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://www.bbbarmanacademy.com/">
+
+<!-- TWITTER META -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@BB Barman Academy">
+<meta name="twitter:creator" content="@BB Barman Academy">
+<meta name="twitter:title" content="BB Barman Academy">
+<meta name="twitter:description" content="BB Barman Academy | Opening a School in India">
+<meta name="twitter:image" content="assets/img/logo.png">
+
+<!-- FAVICON FILES -->
+<link href="assets/img/ico/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon" sizes="144x144">
+<link href="assets/img/ico/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon" sizes="114x114">
+<link href="assets/img/ico/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon" sizes="72x72">
+<link href="assets/img/ico/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon">
+<link href="assets/img/ico/favicon.png" rel="shortcut icon">
+
+<!-- SITEMAP -->
+<link rel=sitemap type="application/xml" title="Sitemap" href="" >
+<!-- CANONICAL -->
+<link rel="canonical" href="http://www.bbbarmanacademy.com/"  />
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+
+        <style type="text/css">
+        	/*********************** Demo - 1 *******************/
+
+
+
+/*********************** Demo - 7 *******************/
+.menu_box:after,.menu_box:before{content:""}
+.menu_box{overflow:hidden}
+.menu_box .title{letter-spacing:1px}
+.menu_box .post{font-style:italic}
+body{background-color:#f1f1f2}
+.menu_box .title{text-transform:uppercase}
+.menu_box{box-shadow:0 0 3px rgba(0,0,0,.3)}
+
+.menu_box .icon{padding:0;list-style:none}
+.menu_box,.menu_box .icon li a{text-align:center}
+
+
+.mt-30{margin-top:30px}
+.mt-40{margin-top:40px}
+.mb-30{margin-bottom:30px}
+.menu_box{position:relative; border-radius: 10px;}
+.menu_box:after,.menu_box:before{width:100%;height:100%;background:rgba(11,33,47,.9);position:absolute;top:0;left:0;opacity:0;transition:all .5s ease 0s}
+.menu_box:after{background:rgba(255,255,255,.3);border:2px solid #0dab76;top:0;left:170%;opacity:1;z-index:1;transform:skewX(45deg);transition:all 1s ease 0s}
+.menu_box:hover:before{opacity:1}
+.menu_box:hover:after{left:-170%}
+.menu_box img{width:100%;height:auto}
+.menu_box .box-content{width:100%;position:absolute;bottom:-100%;left:0;transition:all .5s ease 0s}
+.menu_box:hover .box-content{bottom:30%}
+.menu_box .title{display:block;font-size:22px;font-weight:700;color:#fff;margin:0 0 10px}
+.menu_box .post{display:block;font-size:15px;font-weight:600;color:#fff;margin-bottom:10px}
+.menu_box .icon{margin:0}
+.menu_box .icon li{display:inline-block; background: rgba(255,255,255,0.2);  border-radius: 100px; padding: 10px;}
+.menu_box .icon li a{display:block;width:35px;height:35px;line-height:35px;border-radius:50%;background:#0dab76;font-size:18px;color:#fff;margin-right:10px;transition:all .5s ease 0s}
+svg {
+	width: 18px;
+	fill: #fff;
+}
+
+.menu_box .icon li svg:hover{transform:rotate(360deg)}
+@media only screen and (max-width:990px){.box{margin-bottom:30px}
+}
+
+.ptb-70 {
+	padding-bottom: 70px;
+}
+
+
+
+        </style>
+    </head>
+    <body style="background: #000;">
+       
+       <div  class="container-fluid mt-40 ptb-70">
+            <div class="row mt-30">
+                <div class="col-md-4 col-sm-6 col-sm-push-8">
+                <a href="services.php">
+                    <div class="menu_box">
+                        <img src="index/img/img-1.jpg" alt="">
+                        <div class="box-content">
+                          <span class="post">Discover:</span>
+                            <h3 class="title">Diverse Services</h3>
+                       
+                           
+                            <ul class="icon">
+                               <li><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 15.2 15" enable-background="new 0 0 15.2 15" xml:space="preserve">
+<g>
+	<path d="M7.6,2.8C8.4,2.8,9,2.1,9,1.4S8.3,0,7.5,0C6.7,0,6.1,0.7,6.1,1.4c0,0,0,0,0,0.1C6.2,2.3,6.9,2.9,7.6,2.8z"></path>
+	<path d="M1.5,2.8c0.8,0,1.4-0.7,1.3-1.5S2.1,0,1.4,0C0.6,0,0,0.7,0,1.4c0,0,0,0,0,0.1C0,2.3,0.7,2.9,1.5,2.8z"></path>
+	<path d="M13.8,2.8c0.8,0,1.4-0.7,1.3-1.5S14.4,0,13.7,0h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0,0,0.1
+		C12.3,2.3,13,2.9,13.8,2.8z"></path>
+	<path d="M13.8,8.9c0.8,0,1.4-0.7,1.3-1.5c0-0.8-0.7-1.4-1.5-1.3h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0.1,0,0.1
+		C12.4,8.4,13,9,13.8,8.9z"></path>
+	<path d="M1.4,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1C0,14.4,0.7,15,1.5,15s1.4-0.7,1.3-1.5S2.1,12.1,1.4,12.2z"></path>
+	<path d="M7.5,6.1c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C9,6.6,8.3,6,7.5,6.1z"></path>
+	<path d="M13.7,12.2L13.7,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3s1.4-0.7,1.3-1.5
+		S14.4,12.1,13.7,12.2z"></path>
+	<path d="M1.4,6.1C0.6,6.1,0,6.7,0,7.5c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C2.8,6.6,2.1,6,1.4,6.1z"></path>
+	<path d="M7.5,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3S9,14.3,9,13.5S8.3,12.1,7.5,12.2z"></path>
+</g>
+</svg></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6 col-sm-pull-4" >
+                 <a href="home2.php">
+                    <div class="menu_box">
+                        <img src="index/img/img-2.jpg" alt="">
+                        <div class="box-content">
+                          <span class="post">Discover:</span>
+                            <h3 class="title">Home</h3>
+                       
+                            <ul class="icon">
+                               <li><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 15.2 15" enable-background="new 0 0 15.2 15" xml:space="preserve">
+<g>
+	<path d="M7.6,2.8C8.4,2.8,9,2.1,9,1.4S8.3,0,7.5,0C6.7,0,6.1,0.7,6.1,1.4c0,0,0,0,0,0.1C6.2,2.3,6.9,2.9,7.6,2.8z"></path>
+	<path d="M1.5,2.8c0.8,0,1.4-0.7,1.3-1.5S2.1,0,1.4,0C0.6,0,0,0.7,0,1.4c0,0,0,0,0,0.1C0,2.3,0.7,2.9,1.5,2.8z"></path>
+	<path d="M13.8,2.8c0.8,0,1.4-0.7,1.3-1.5S14.4,0,13.7,0h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0,0,0.1
+		C12.3,2.3,13,2.9,13.8,2.8z"></path>
+	<path d="M13.8,8.9c0.8,0,1.4-0.7,1.3-1.5c0-0.8-0.7-1.4-1.5-1.3h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0.1,0,0.1
+		C12.4,8.4,13,9,13.8,8.9z"></path>
+	<path d="M1.4,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1C0,14.4,0.7,15,1.5,15s1.4-0.7,1.3-1.5S2.1,12.1,1.4,12.2z"></path>
+	<path d="M7.5,6.1c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C9,6.6,8.3,6,7.5,6.1z"></path>
+	<path d="M13.7,12.2L13.7,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3s1.4-0.7,1.3-1.5
+		S14.4,12.1,13.7,12.2z"></path>
+	<path d="M1.4,6.1C0.6,6.1,0,6.7,0,7.5c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C2.8,6.6,2.1,6,1.4,6.1z"></path>
+	<path d="M7.5,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3S9,14.3,9,13.5S8.3,12.1,7.5,12.2z"></path>
+</g>
+</svg></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="clearfix"></div>
+
+                <div style="margin-top: 4em; margin-bottom: 4em;" class="col-md-6 col-sm-6 col-sm-offset-3">
+                 <a href="consulting.php">
+                    <div class="menu_box">
+                        <img src="index/img/img-3.jpg" alt="">
+                        <div class="box-content">
+                          <span class="post">Discover:</span>
+                            <h3 class="title">Consulting</h3>
+                          
+                            <ul class="icon">
+                                <li><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 15.2 15" enable-background="new 0 0 15.2 15" xml:space="preserve">
+<g>
+	<path d="M7.6,2.8C8.4,2.8,9,2.1,9,1.4S8.3,0,7.5,0C6.7,0,6.1,0.7,6.1,1.4c0,0,0,0,0,0.1C6.2,2.3,6.9,2.9,7.6,2.8z"></path>
+	<path d="M1.5,2.8c0.8,0,1.4-0.7,1.3-1.5S2.1,0,1.4,0C0.6,0,0,0.7,0,1.4c0,0,0,0,0,0.1C0,2.3,0.7,2.9,1.5,2.8z"></path>
+	<path d="M13.8,2.8c0.8,0,1.4-0.7,1.3-1.5S14.4,0,13.7,0h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0,0,0.1
+		C12.3,2.3,13,2.9,13.8,2.8z"></path>
+	<path d="M13.8,8.9c0.8,0,1.4-0.7,1.3-1.5c0-0.8-0.7-1.4-1.5-1.3h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0.1,0,0.1
+		C12.4,8.4,13,9,13.8,8.9z"></path>
+	<path d="M1.4,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1C0,14.4,0.7,15,1.5,15s1.4-0.7,1.3-1.5S2.1,12.1,1.4,12.2z"></path>
+	<path d="M7.5,6.1c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C9,6.6,8.3,6,7.5,6.1z"></path>
+	<path d="M13.7,12.2L13.7,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3s1.4-0.7,1.3-1.5
+		S14.4,12.1,13.7,12.2z"></path>
+	<path d="M1.4,6.1C0.6,6.1,0,6.7,0,7.5c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C2.8,6.6,2.1,6,1.4,6.1z"></path>
+	<path d="M7.5,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3S9,14.3,9,13.5S8.3,12.1,7.5,12.2z"></path>
+</g>
+</svg></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+                
+                <div class="col-md-4 col-sm-6 col-sm-push-8">
+                 <a href="projects.php">
+                    <div class="menu_box">
+                        <img src="index/img/img-1.jpg" alt="">
+                        <div class="box-content">
+
+                         <span class="post">Discover:</span>
+                            <h3 class="title">Projects</h3>
+                           
+                            <ul class="icon">
+                               <li><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 15.2 15" enable-background="new 0 0 15.2 15" xml:space="preserve">
+<g>
+	<path d="M7.6,2.8C8.4,2.8,9,2.1,9,1.4S8.3,0,7.5,0C6.7,0,6.1,0.7,6.1,1.4c0,0,0,0,0,0.1C6.2,2.3,6.9,2.9,7.6,2.8z"></path>
+	<path d="M1.5,2.8c0.8,0,1.4-0.7,1.3-1.5S2.1,0,1.4,0C0.6,0,0,0.7,0,1.4c0,0,0,0,0,0.1C0,2.3,0.7,2.9,1.5,2.8z"></path>
+	<path d="M13.8,2.8c0.8,0,1.4-0.7,1.3-1.5S14.4,0,13.7,0h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0,0,0.1
+		C12.3,2.3,13,2.9,13.8,2.8z"></path>
+	<path d="M13.8,8.9c0.8,0,1.4-0.7,1.3-1.5c0-0.8-0.7-1.4-1.5-1.3h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0.1,0,0.1
+		C12.4,8.4,13,9,13.8,8.9z"></path>
+	<path d="M1.4,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1C0,14.4,0.7,15,1.5,15s1.4-0.7,1.3-1.5S2.1,12.1,1.4,12.2z"></path>
+	<path d="M7.5,6.1c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C9,6.6,8.3,6,7.5,6.1z"></path>
+	<path d="M13.7,12.2L13.7,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3s1.4-0.7,1.3-1.5
+		S14.4,12.1,13.7,12.2z"></path>
+	<path d="M1.4,6.1C0.6,6.1,0,6.7,0,7.5c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C2.8,6.6,2.1,6,1.4,6.1z"></path>
+	<path d="M7.5,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3S9,14.3,9,13.5S8.3,12.1,7.5,12.2z"></path>
+</g>
+</svg></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6 col-sm-pull-4">
+                 <a href="blog.php">
+                    <div class="menu_box">
+                        <img src="index/img/img-2.jpg" alt="">
+                        <div class="box-content">
+                         <span class="post">Discover:</span>
+                            <h3 class="title">Blog</h3>
+                            <ul class="icon">
+                               <li><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 15.2 15" enable-background="new 0 0 15.2 15" xml:space="preserve">
+<g>
+	<path d="M7.6,2.8C8.4,2.8,9,2.1,9,1.4S8.3,0,7.5,0C6.7,0,6.1,0.7,6.1,1.4c0,0,0,0,0,0.1C6.2,2.3,6.9,2.9,7.6,2.8z"></path>
+	<path d="M1.5,2.8c0.8,0,1.4-0.7,1.3-1.5S2.1,0,1.4,0C0.6,0,0,0.7,0,1.4c0,0,0,0,0,0.1C0,2.3,0.7,2.9,1.5,2.8z"></path>
+	<path d="M13.8,2.8c0.8,0,1.4-0.7,1.3-1.5S14.4,0,13.7,0h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0,0,0.1
+		C12.3,2.3,13,2.9,13.8,2.8z"></path>
+	<path d="M13.8,8.9c0.8,0,1.4-0.7,1.3-1.5c0-0.8-0.7-1.4-1.5-1.3h0c-0.8,0-1.4,0.7-1.3,1.4c0,0,0,0.1,0,0.1
+		C12.4,8.4,13,9,13.8,8.9z"></path>
+	<path d="M1.4,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1C0,14.4,0.7,15,1.5,15s1.4-0.7,1.3-1.5S2.1,12.1,1.4,12.2z"></path>
+	<path d="M7.5,6.1c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C9,6.6,8.3,6,7.5,6.1z"></path>
+	<path d="M13.7,12.2L13.7,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3s1.4-0.7,1.3-1.5
+		S14.4,12.1,13.7,12.2z"></path>
+	<path d="M1.4,6.1C0.6,6.1,0,6.7,0,7.5c0,0,0,0.1,0,0.1c0,0.8,0.7,1.4,1.5,1.3c0.8,0,1.4-0.7,1.3-1.5
+		C2.8,6.6,2.1,6,1.4,6.1z"></path>
+	<path d="M7.5,12.2c-0.8,0-1.4,0.7-1.4,1.4c0,0,0,0,0,0.1c0,0.8,0.7,1.4,1.5,1.3S9,14.3,9,13.5S8.3,12.1,7.5,12.2z"></path>
+</g>
+</svg></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+      
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    </body>
+</html>
